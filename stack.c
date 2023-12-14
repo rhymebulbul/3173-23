117be8021f0202e8a3ef8dce2fa63363342beece
Rhyme Bulbul <rbul0005@student.monash.edu>
2023-12-14 23:15:57 +1100

    Add

--- stack.c
+++ stack.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int bof(char *str,int studentID)
+int bof(char *str, int studentID)
 {
     int bufSize;
-	int a = 12;
-	int b = 18;
-	
-    bufSize = 12 + studentID%32;
-    
+    int a = 12;
+    int b = 18;
+
+    bufSize = 12 + studentID % 32; //20
+
     char buffer[bufSize];
 
     strcpy(buffer, str);
@@ -20,14 +20,15 @@ int bof(char *str,int studentID)
 int main(int argc, char **argv[])
 {
 
-if(argc < 2) {
-                printf("Usage: %s <paylaod>\n", argv[0]);
-                exit(0);
-}
+    if (argc < 2)
+    {
+        printf("Usage: %s <paylaod>\n", argv[0]);
+        exit(0);
+    }
 
-    int studentID = 31865224; //ENTER YOUR STUDENT ID HERE
+    int studentID = 31865224; // ENTER YOUR STUDENT ID HERE
 
-    bof(argv[1],studentID);
+    bof(argv[1], studentID);
 
     printf("Returned Properly\n");
     return 1;
